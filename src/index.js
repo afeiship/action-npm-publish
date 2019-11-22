cb59ad13d218195eac4af54c4ef44f5d6d9638b3
afeiship <1290657123@qq.com>
2019-11-22 17:42:49 +0800

    fix: /Users/feizheng to ~

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ async function main() {
   };
 
   fs.writeFileSync(
-    `$HOME/.npmrc`,
+    `~/.npmrc`,
     `//registry.npmjs.org/:_authToken=${process.env.NPM_AUTH_TOKEN}`
   );
-  await exec.exec('cat', [`$HOME/.npmrc`]);
+  await exec.exec('cat', [`~/.npmrc`]);
   await exec.exec('ls', ['-alh']);
 
   await exec.exec('npm', ['publish', '--access=public']);
